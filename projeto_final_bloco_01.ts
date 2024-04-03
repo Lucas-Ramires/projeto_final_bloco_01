f5e0ad086e38602fbf80a2f8f5689b6db2d26c39
kotecbr@outlook.com <kotecbr@outlook.com>
2024-04-03 11:12:02 -0300

    Criação inicial do menu e adicionando colors

---
+++ projeto_final_bloco_01.ts
@@ -0,0 +1,90 @@
+﻿import leia = require("readline-sync");
+
+import { colors } from "./src/util/Colors";
+
+
+export function main() {
+
+    let opcao, tipo, preco: number;
+    let nome, perifericos, hardwares: string;
+    let tipoProduto = [`Periferico`,`Hardware`]
+    
+
+    while (true) {
+
+        console.log(colors.bg.black, colors.fg.yellow,
+            "*****************************************************");
+        console.log("                                                     ");
+        console.log("                     Kilobyte                        ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("            1 - Cadastrar Produto                    ");
+        console.log("            2 - Listar todos os Produtos             ");
+        console.log("            3 - Listar Produto pelo ID               ");
+        console.log("            4 - Atualizar Produto                    ");
+        console.log("            5 - Deletar Produto                      ");
+        console.log("            0 - Sair                                 ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ",
+            colors.reset);
+
+        console.log("Entre com a opção desejada: ");
+        opcao = leia.questionInt("");
+
+        if (opcao == 0) {
+            console.log(colors.fg.greenstrong,
+                "\nKilobyte Store - Perifericos e Hardwares sao aqui!");
+            sobre();
+            console.log(colors.reset, "");
+            process.exit(0);
+        }
+
+        switch (opcao) {
+            case 1:
+                
+            case 2:
+                
+                break;
+            case 3:
+            
+
+           
+                break;
+            case 4:
+              
+                break;
+            case 5:
+
+                break;
+            default:
+                console.log(colors.fg.whitestrong,
+                    "\nOpção Inválida!\n", colors.reset);
+
+                keyPress()
+                break;
+        }
+    }
+
+}
+
+function sobre(): void {
+    console.log(colors.bg.black, colors.fg.greenstrong,
+                "                                                       ");
+    console.log("*******************************************************");
+    console.log("Projeto Desenvolvidor por: Lucas Ramires");
+    console.log("E-mail: kotecbr@outlook.com");
+    console.log("https://github.com/Lucas-Ramires");
+    console.log("*******************************************************")
+    console.log("                                                       ",
+    colors.reset);
+}
+
+function keyPress(): void {
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    leia.prompt();
+}
+
+main();
